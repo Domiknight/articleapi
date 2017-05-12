5ad50020e9dc81cf00b109f986cc55980a8c9975
David Quaglieri <david@websitesonq.com.au>
2017-05-12 20:28:33 +1000

    Vagrant box package install

--- Vagrantfile
+++ Vagrantfile
@@ -69,4 +69,7 @@ Vagrant.configure("2") do |config|
   #   apt-get update
   #   apt-get install -y apache2
   # SHELL
+
+  # PROJECT PROVISIONING
+  config.vm.provision :shell, path: "scripts/bootstrap.sh"
 end
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Additional Repos
+## php 5.6
+apt-get install -y software-properties-common
+apt-get install -y python-software-properties
+add-apt-repository -y ppa:ondrej/php
+
+# Update
+apt-get update
+
+# Apache
+apt-get install -y apache2
+
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+  ln -fs /vagrant/web /var/www/html
+fi
+
+# PHP
+apt-get install -y php5.6-cli php5.6 php5.6-mysql php5.6-mbstring php5.6-curl php5.6-common php5.6-xml php5.6-pdo php5.6-dom php5.6-intl
+
+# MySQL
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password Password'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Password'
+apt-get install -y mysql-server
+
+# Git & zip
+apt-get install -y git zip unzip
+
+# load the changes
+a2enmod rewrite
+service apache2 restart
