90ac1728d33da36bbba70e3a0365f345a1ab991c
David Quaglieri <david@websitesonq.com.au>
2017-05-14 22:34:18 +1000

    apache conf for vagrant provisioning

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -15,21 +15,21 @@ fi
 
 # Apache
 if ! [ -f /etc/apache2/sites-available/000-default.conf.orig ]; then
-    # Update the default security model to point to /vagrant share
-    cp /etc/apache2/apache2.conf /etc/apache2/apache.conf.orig
-    sed -e 's/Directory \/var\/www/Directory \/vagrant/' < /etc/apache2/apache2.conf > /etc/apache2/apache2.conf.new
-    mv /etc/apache2/apache2.conf.new /etc/apache2/apache2.conf
-
-    # Point the Document Root to the right place
-    cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.orig
-    sed -e 's/DocumentRoot \/var\/www\/html/DocumentRoot \/vagrant\/web/' < /etc/apache2/sites-available/000-default.conf > /etc/apache2/sites-available/000-default.conf.new
-    mv /etc/apache2/sites-available/000-default.conf.new /etc/apache2/sites-available/000-default.conf
 
     # Make apache run as the vagrant user - solves perms issues with shared folders
     echo "Make apache run as vagrant user"
     cp /etc/apache2/envvars /etc/apache2/envvars.orig
     sed -e 's/www\-data/vagrant/' < /etc/apache2/envvars > /etc/apache2/envvars.new
     mv /etc/apache2/envvars.new /etc/apache2/envvars
+
+    # Update the default security model to point to /vagrant share
+    cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.orig
+    sed -e 's/Directory \/var\/www/Directory \/vagrant/' < /etc/apache2/apache2.conf > /etc/apache2/apache2.conf.new
+    mv /etc/apache2/apache2.conf.new /etc/apache2/apache2.conf
+
+    # Point the Document Root to the right place
+    cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.orig
+    cp /vagrant/scripts/apache/000-default.conf /etc/apache2/sites-available/000-default.conf
 fi
 
 # load the changes
